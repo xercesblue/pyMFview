eced4e6465e4da964c6d95f5136a60f05e02ec69
xerces <dlcs.frank@gmail.com>
2011-10-23 15:53:47 -0400

    Overhauled viewer. Now takes parameters.

--- viewer.py
+++ viewer.py
@@ -1,76 +1,165 @@
+#Author: Francisco De La Cruz <dlcs.frank@gmail.com>
+# Date: October 19, 2011
+
+# libdect result viewer.
+# This viewer has only been tested with the MF detector.
+# Other detectors should work, mainly RX and RX adaptive
+# 
 
 import struct
 from PIL import Image
 import ImageEnhance
+import sys
+import getopt
+# Parameters
 
-im = Image.new("RGB", (719,1440))
-pixels = im.load()
-ff = []
-threshold = 1
-target_epsi = 0.01
-tmean_adjust = 0.009
-ff_target = []
-# Read Target Mean
-with open("mean.dat", "rb") as t:
-    byte = t.read(4)
-    while byte != "":
-        # Do stuff with byte.
-	try:
-            tb = t.read(4)
-            if len(tb) == 0:
-                print "end of target data"
-                break
-            ftar ,  = struct.unpack('<f', tb)
-            ff_target.append(ftar)
-	except EOFError:
-            print "done reading target data"
+def usage():
+    print "libdect result viewer usage"
+    print "-o <output file>"
+    print "-i <input file>"
+    print "-m <mean file>"
+    print "-t <threshold>"
+    print "-s <date dimensions> i.e. -s 200, 200"
 
 
-#target mean
-print "Read %s target values " % len(ff)
-tmean = sum(ff_target)/len(ff_target)
+def viewer(argv):
+ 
+    try:
+        optlist, args = getopt.getopt(argv, "t:m:i:o:s:",["threshold", "mean=","input=","output=","size="])
+        if not optlist:
+            usage()
+            sys.exit(2)
+    except getopt.GetoptError, err:
+        usage()
+        sys.exit(2)
+    
+    im = None
+    output = None
+    input = None
+    size = None
+    mean_file = None
+    threshold = 0.5
+    for o, a in optlist:
+        print o, a
+    
+        if o in ("-i", "--input") and a:
+             input = a
+        elif o in ("-o", "--output"):
+            output = a
+        elif o in ("-s", "--size"):
+            try:
+                w, h =a.split(',')
+                size = (int(w), int(h))
+            except ValueError:
+                print "Invalid dimensions"
+                usage()
+                sys.exit(2)
+        elif o in ("-m", "mean"):
+            if a:
+                mean_file = a
+                print "Using mean file", mean_file
+        elif o in ("-t", "threshold"): 
+             if a:
+                try:
+                     threshold = float(a)
+                except ValueError:
+                    print "Invalid threshold"
+                    usage()
+                    sys.exit(2)
+        # General error
+        else:
+           usage()
+           sys.exit(2)
 
-print "target %s" % tmean
-print "setting threshold to %s"%tmean
-threshold =  tmean-tmean_adjust
+    if size:
+        im = Image.new("RGB", size)
+    else:
+       usage()
+       sys.exit(2)
+   
+    # Load image
+    pixels = im.load()
+    data_values = []
+    target_epsi = 0.01
+    tmean_adjust = 0.009
+    mean_values = []
 
-# Read Results
-with open("MF_gpu.dat", "rb") as f:
-    byte = f.read(4)
-    while byte != "":
-        # Do stuff with byte.
-	try:
-            b = f.read(4)
-            if len(b) == 0:
-                print "end of data"
-                break
-            flo ,  = struct.unpack('<f', b)
-            ff.append(flo)
-	except EOFError:
-            print "done reading results"
-print "Read %s res values " % len(ff)
-mean = sum(ff)/len(ff)
-print "mean %s" % mean
+ 
+    # Check command line arguments
+    # Read Target Mean
+    if mean_file:
+        try:
+            with open(mean_file, "rb") as t:
+                byte = t.read(4)
+                while byte != "":
+                    # Do stuff with byte.
+                    try:
+                        readb = t.read(4)
+                        if len(readb) == 0:
+                            print "End of mean data"
+                            break
+                        meanval ,  = struct.unpack('<f', readb)
+                        mean_values.append(meanval)
+                    except EOFError:
+                        print "End of mean data"
 
-print "mean -> threshold delta %s" % (mean-threshold)
-for i in range(im.size[0]):    # for every pixel:
-    for j in range(im.size[1]):
-	fval = ff.pop()
-	
-	if fval > threshold:
-            pixels[i,j] = 255, 255, 255
-	elif abs(fval-tmean) > target_epsi:
-            #		pixels[i,j] = 83, 128, 17
-            pixels[i,j] = 200, 128, 17
-	else:
-            pixels[i,j] = 90, 90, 90
+                #target mean
+                print "Read %s mean values " % len(mean_values)
+                tmean = sum(mean_values)/len(mean_values)
+
+                print "Target mean calculated at %s" % tmean
+                threshold =  tmean-tmean_adjust
+           
+        except IOError:
+            print "File not found", mean_file
+            sys.exit(2)
+
+    print "Threshold set at ", threshold
+ 
+        # Read Results
+    try:
+
+        with open(input, "rb") as f:
+            byte = f.read(4)
+            while byte != "":
+                # Do stuff with byte.
+                try:
+                    b = f.read(4)
+                    if len(b) == 0:
+                        print "End of data set"
+                        break
+                    meanval ,  = struct.unpack('<f', b)
+                    data_values.append(meanval)
+                except EOFError:
+                    print "Done reading data"
+    except IOError:
+        print "File not found", input
+        sys.exit(2)
+
+    print "Read %s data values " % len(data_values)
+    mean = sum(data_values)/len(data_values)
+    print "Data mean %s" % mean
 
 
-im.show()
+    print "mean -> threshold delta %s" % (mean-threshold)
+    for i in range(im.size[0]):    # for every pixel:
+        for j in range(im.size[1]):
+            fval = ff.pop()
 
-im.save("img.png", format="PNG")	
+            if fval > threshold:
+                pixels[i,j] = 255, 255, 255
+            elif abs(fval-tmean) > target_epsi:
+                #		pixels[i,j] = 83, 128, 17
+                pixels[i,j] = 200, 128, 17
+            else:
+                pixels[i,j] = 90, 90, 90
 
 
+    im.show()
+    im.save(output, format="PNG")	
+
+if __name__ == "__main__":
+    viewer(sys.argv[1:])
 
 
 
